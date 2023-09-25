52771da46417e67ec8d80655a990d6f1e1ba156b
Wagner Ribas <59660174+wagnerr63@users.noreply.github.com>
2023-09-25 00:28:56 +0000

    update main

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from database.db import Database
 from repository.client import ClientRepository
 from repository.activity import ActivityRepository
+from repository.room import RoomRepository
 from entity.activity import Activity
 from entity.client import Client
+from entity.room import Room
 
-if __name__ == '__main__':
+if __name__ == '_main_':
     db = Database()
     db.connect()
 
     clientRepo = ClientRepository()
     activityRepo = ActivityRepository()
+    roomRepo = RoomRepository()
 
     option = -1
     while option != 0:
@@ -17,9 +20,12 @@ if __name__ == '__main__':
         print("2 - Listar clientes")
         print("3 - Excluir cliente")
         print("4 - Atualizar cliente")
+        print("5 - Criar atividade")
+        print("6 - Listar atividades")
+        print("7 - Excluir atividade")
+        print("8 - Atualizar atividade")
+        print("9 - Criar quarto")
         print("0 - Sair")
-
-
         option = int(input())
 
         if option == 1:
@@ -89,6 +95,46 @@ if __name__ == '__main__':
 
             clientRepo.update(clientByID)
             print("Cliente atualizado com sucesso!")
+
+        if option == 5:
+            newActivity = Activity.new()
+            print("Informe os dados da atividade:\n")
+            print("Nome:")
+            newActivity.name = input()
+            print("Local:")
+            newActivity.local = input()
+            print("Descrição:")
+            newClient.description = input()
+            
+            activityRepo.insert(newActivity)
+            print("Atividade inserido com sucesso!")
+
+        if option == 6:
+            print("Atividades: ")
+            allActivity = activityRepo.list()
+            for activity in allActivity:
+                print(activity)
+
+        if option == 7:
+            print("Informe o ID da atividade: ")
+            id = input()
+            activityRepo.delete(id)
+            print("Atividade excluido com sucesso!")
+
+        if option == 9:
+            newRoom = Room.new()
+            print("Informe os dados do quarto:\n")
+            print("Quantidade:")
+            newActivity.name = input()
+            print("Local:")
+            newActivity.local = input()
+            print("Descrição:")
+            newClient.description = input()
+            
+            activityRepo.insert(newActivity)
+            print("Atividade inserido com sucesso!")
+
+        
 
 
     print("Saindo...")
