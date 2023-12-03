f14ffab72f756a73a87e858a10cb2d4ec7b444e9
wagnerribas <wagner.decastilho@br.experian.com>
2023-12-03 20:26:24 -0300

    neo: activity find by id

--- repository/neo/activity.py
+++ repository/neo/activity.py
@@ -1,3 +1,5 @@
+from typing import Type
+
 from entity.activity import Activity
 from database.neo4jdb import Neo4JDB
 
@@ -24,3 +26,16 @@ class ActivityRepository:
     def delete(self, id: str):
         query = "MATCH (n:activity {id:$id}) DELETE n"
         self.db.session.run(query, parameters={'id': id})
+
+    def find_by_id(self, id: str) -> Type[Activity]:
+        query = "MATCH (n:activity {id:$id}) RETURN n.id, n.name, n.local, n.description"
+        result = self.db.session.run(query, parameters={'id': id})
+        row = result.values()[0]
+
+        activityByID = Activity
+        activityByID.id = row[0]
+        activityByID.name = row[1]
+        activityByID.local = row[2]
+        activityByID.description = row[3]
+        return activityByID
+
