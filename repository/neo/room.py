2140958b4509d4c614e0c8e05c0c673c9b7b9b96
wagnerribas <wagner.decastilho@br.experian.com>
2023-12-03 22:47:26 -0300

    room: fix update

--- repository/neo/room.py
+++ repository/neo/room.py
@@ -14,7 +14,7 @@ class RoomRepository:
 
     def insert(self, room: Room):
         query = "CREATE (n:room {id:$id, name:$name, qty_beds:$qty_beds, qty_restrooms:$qty_restrooms, hydromassage:$hydromassage, description:$description, price:$price})" 
-        self.db.session.run(query, parameters={'id': room.id, 'name': room.name, 'qty_beds': room.qty_beds, 'qty_restrooms': room.qty_restrooms, 'hydromassage': room.hydromassagem, 'description': room.description, 'price': room.price})
+        self.db.session.run(query, parameters={'id': room.id, 'name': room.name, 'qty_beds': room.qty_beds, 'qty_restrooms': room.qty_restrooms, 'hydromassage': room.hydromassage, 'description': room.description, 'price': room.price})
 
     def list(self):
         query = "MATCH (n:room) RETURN n.id, n.name, n.qty_beds, n.qty_restrooms, n.hydromassage, n.description, n.price LIMIT 100"
@@ -28,7 +28,7 @@ class RoomRepository:
 
     def update(self, room: Room):
         query = "MATCH (n:room {id:$id}) SET n.name=$name, n.qty_beds=$qty_beds, n.qty_restrooms=$qty_restrooms, n.hydromassage=$hydromassage, n.description=$description, n.price=$price"
-        self.db.session.run(query, parameters={'id': id, 'name': room.name, 'qty_beds': room.qty_beds, 'qty_restrooms': room.qty_restrooms, 'hydromassage': room.hydromassage, 'description': room.description, 'price': room.price})
+        self.db.session.run(query, parameters={'id': room.id, 'name': room.name, 'qty_beds': room.qty_beds, 'qty_restrooms': room.qty_restrooms, 'hydromassage': room.hydromassage, 'description': room.description, 'price': room.price})
 
     def find_by_id(self, id: str) -> Type[Room]:
         query = "MATCH (n:room {id:$id}) RETURN n.id, n.name, n.description, n.qty_beds, n.qty_restrooms, n.hydromassage, n.price"
