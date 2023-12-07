f15e96b2a49979ea1475ddf83ca4ddd220fcc62d
wagnerribas <wagner.decastilho@br.experian.com>
2023-12-06 21:16:04 -0300

    room: fix ids

--- repository/neo/room.py
+++ repository/neo/room.py
@@ -13,22 +13,37 @@ class RoomRepository:
         self.db.connect()
 
     def insert(self, room: Room):
-        query = "MATCH (r:room) WITH r ORDER BY r.roomID LIMIT 1 DESC CREATE (n:room {roomID:toString(toInteger(r.id)+1), name:$name, qty_beds:$qty_beds, qty_restrooms:$qty_restrooms, hydromassage:$hydromassage, description:$description, price:$price})"
-        self.db.session.run(query, parameters={'name': room.name, 'qty_beds': room.qty_beds, 'qty_restrooms': room.qty_restrooms, 'hydromassage': room.hydromassage, 'description': room.description, 'price': room.price})
+        last_id = self.find_last_id()
+        room_id = last_id + 1
+        query = ("MATCH (r:room) WITH r ORDER BY r.roomID LIMIT 1 DESC CREATE (n:room {roomID:$roomID, name:$name, "
+                 "qty_beds:$qty_beds, qty_restrooms:$qty_restrooms, hydromassage:$hydromassage, "
+                 "description:$description, price:$price})")
+        self.db.session.run(query, parameters={'roomID': room_id, 'name': room.name, 'qty_beds': room.qty_beds,
+                                               'qty_restrooms': room.qty_restrooms, 'hydromassage': room.hydromassage,
+                                               'description': room.description, 'price': room.price})
+
+    def find_last_id(self) -> int:
+        query = "match (n:room) return n.roomID ORDER BY n.roomID DESC LIMIT 1"
+        result = self.db.session.run(query)
+        if not result.peek():
+            return 0
+        return int(result.single()[0])
 
     def list(self):
-        query = "MATCH (n:room) RETURN n.id, n.name, n.qty_beds, n.qty_restrooms, n.hydromassage, n.description, n.price LIMIT 100"
+        query = "MATCH (n:room) RETURN n.roomID, n.name, n.qty_beds, n.qty_restrooms, n.hydromassage, n.description, n.price LIMIT 100"
         results = self.db.session.run(query)
         rooms = results.values()
         return rooms
-    
+
     def delete(self, id: str):
-        query = "MATCH (n:room {id:$id}) DELETE n"
+        query = "MATCH (n:room {roomID:$id}) DELETE n"
         self.db.session.run(query, parameters={'id': id})
 
     def update(self, room: Room):
-        query = "MATCH (n:room {id:$id}) SET n.name=$name, n.qty_beds=$qty_beds, n.qty_restrooms=$qty_restrooms, n.hydromassage=$hydromassage, n.description=$description, n.price=$price"
-        self.db.session.run(query, parameters={'id': room.id, 'name': room.name, 'qty_beds': room.qty_beds, 'qty_restrooms': room.qty_restrooms, 'hydromassage': room.hydromassage, 'description': room.description, 'price': room.price})
+        query = "MATCH (n:room {roomID:$id}) SET n.name=$name, n.qty_beds=$qty_beds, n.qty_restrooms=$qty_restrooms, n.hydromassage=$hydromassage, n.description=$description, n.price=$price"
+        self.db.session.run(query, parameters={'id': room.id, 'name': room.name, 'qty_beds': room.qty_beds,
+                                               'qty_restrooms': room.qty_restrooms, 'hydromassage': room.hydromassage,
+                                               'description': room.description, 'price': room.price})
 
     def find_by_id(self, id: str) -> Type[Room]:
         query = "MATCH (n:room {roomID:$id}) RETURN n.id, n.name, n.description, n.qty_beds, n.qty_restrooms, n.hydromassage, n.price"
